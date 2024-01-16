5801b11c6d99913a7d0cf330d1c68825c216d1e7
Ayanda Ngubane <ayanda4me@gmail.com>
2024-01-16 04:57:22 -0800

    Placd

--- models/place.py
+++ models/place.py
@@ -36,7 +36,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
-        reviews = relationship('Review', backref='place',
+        reviews = relationship('Review', backref='places',
                                cascade='all, delete, delete-orphan')
         amenities = relationship('Amenity', secondary=place_amenity,
                                  viewonly=False, backref='place_amenities')
